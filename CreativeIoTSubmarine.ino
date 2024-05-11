5bbb82323787448d4398ed4bf171d29179cf7061
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-05-11 11:35:01 +0800

    Remove redundant leftover of analog temp sensor code

--- CreativeIoTSubmarine.ino
+++ CreativeIoTSubmarine.ino
@@ -26,16 +26,6 @@
 OneWire wire(T_SENS_PIN);
 DallasTemperature tempsens(&wire);
 
-/* ----------------- ANALOG TEMP SENSOR ----------------------*/
-// Temp sensor pin
-const int ThermistorPin = A0;
-int Vo;
-
-const float c1 = 0.001129148, c2 = 0.000234125, c3 = 0.0000000876741; //steinhart-hart coeficients for thermistor
-float R1 = 10300; // value of R1 on board
-float logR2, R2, T;
-/* ---------------------------------------------------------*/
-
 char ssid[] = WIFI_SSID;
 char password[] = WIFI_PASSWORD;
 
